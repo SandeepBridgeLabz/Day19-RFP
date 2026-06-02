082839d35b89b4b9aef327a466e10ebc0c19fbad
sandeep <sandeepmishra898k@gmail.com>
2026-06-02 13:54:39 +0530

    UC7 - Password Rule 3

--- App/src/UserRegistration.java
+++ App/src/UserRegistration.java
@@ -23,11 +23,14 @@ public class UserRegistration {
     public boolean validatePasswordRule2(String password) {
         return Pattern.matches("^(?=.*[A-Z]).{8,}$", password);
     }
+    public boolean validatePasswordRule3(String password) {
+        return Pattern.matches("^(?=.*[A-Z])(?=.*[0-9]).{8,}$", password);
+    }
 
     public static void main(String[] args) {
         UserRegistration user = new UserRegistration();
 
-        System.out.println(user.validatePasswordRule2("Password"));
-        System.out.println(user.validatePasswordRule2("password"));
+        System.out.println(user.validatePasswordRule3("Password1"));
+        System.out.println(user.validatePasswordRule3("Password"));
     }
 }
