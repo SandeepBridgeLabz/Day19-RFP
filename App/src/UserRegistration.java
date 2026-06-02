cad4c1c2f314bc0d37b9d4202109cc1e6632229b
sandeep <sandeepmishra898k@gmail.com>
2026-06-02 13:44:56 +0530

    UC2 - Last Name Validation

--- App/src/UserRegistration.java
+++ App/src/UserRegistration.java
@@ -5,11 +5,14 @@ public class UserRegistration {
     public boolean validateFirstName(String firstName) {
         return Pattern.matches("^[A-Z][a-zA-Z]{2,}$", firstName);
     }
+    public boolean validateLastName(String lastName) {
+        return Pattern.matches("^[A-Z][a-zA-Z]{2,}$", lastName);
+    }
 
     public static void main(String[] args) {
         UserRegistration user = new UserRegistration();
 
-        System.out.println(user.validateFirstName("Kiran"));
-        System.out.println(user.validateFirstName("ki"));
+        System.out.println(user.validateLastName("Kumar"));
+        System.out.println(user.validateLastName("ku"));
     }
 }
